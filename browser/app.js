520b9dc9a8b1561c1d52a18db86a19c03b0fa7db
Navjot <nsingh3104@gmail.com>
2016-03-28 11:53:28 -0400

    finished with persistence

--- browser/app.js
+++ browser/app.js
@@ -1,4 +1,32 @@
-window.whiteboard.on('draw', function(payload){
-	console.log(payload);
-}) ;
 
+var socket = io(window.location.origin);
+socket.on('connect', function(){
+	console.log('I have made a persistent two-way connection to the server!');
+});
+
+
+
+window.whiteboard.on('draw', function(start, end, color){
+	socket.emit('drawing', start, end, color);
+	//console.log('payLoad', payload);
+});
+
+socket.on('drawing', function(start, end, color){
+	window.whiteboard.draw(start, end, color);
+});
+
+socket.on('broadcasting', function(start, end, color){
+	//console.dir(payload);
+	window.whiteboard.draw(start, end, color);
+});
+
+socket.on('refreshdraw', function(cordArray){
+	cordArray.forEach(function(ele){
+		window.whiteboard.draw(ele.start, ele.end, ele.color);
+	});
+});
+
+// cordArray.forEach(function(ele){
+// 	console.log('im inside the forEach!');
+// 	socket.emit('drawing', ele.start, ele.end, ele.color);
+// });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var socketio = require('socket.io');
 
 var http = require('http');
 var server = http.createServer();
@@ -7,6 +8,29 @@ var express = require('express');
 var app = express();
 
 server.on('request', app);
+
+var io = socketio(server);
+
+var counter = 0;
+
+var cordArray = [];
+io.on('connection', function(socket){
+  console.log('A new client has connected');
+  console.log(socket.id);
+  console.log(cordArray);
+  socket.emit('refreshdraw', cordArray);
+  socket.on('disconnect', function(){
+    console.log('A client has disconnected');
+  });
+  socket.on('drawing', function(start, end, color){
+    console.log(++counter);
+    socket.broadcast.emit('broadcasting', start, end, color);
+    cordArray.push({start: start, end: end, color: color});
+    //console.log(cordArray);
+  });
+});
+
+
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
